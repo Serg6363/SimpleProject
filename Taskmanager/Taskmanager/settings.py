729bf09d60c3743b77faa1543b65ffd991b16571
sergei <qqq14582@mail.ru>
2022-10-10 20:32:34 +0300

    Test project. Django - Task manager.

--- Taskmanager/Taskmanager/settings.py
+++ Taskmanager/Taskmanager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Taskmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+
+EMAIL_PORT = 465
+
+EMAIL_HOST_USER = 'mi5sing'
+
+EMAIL_HOST_PASSWORD = 'onhuigpvxruyctvn'
+
+EMAIL_USE_SSL = True
+
--- Taskmanager/main/views.py
+++ Taskmanager/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 
 def index(request):
@@ -14,6 +16,24 @@ def make_task(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
+
+            html_content = render_to_string(
+                'main/task_created.html',
+                {
+                    'task': TaskForm,
+                }
+            )
+
+            msg = EmailMultiAlternatives(
+                subject=f'Новая запись: {form.cleaned_data["title"]}',
+                body=form.cleaned_data['task'],
+                from_email='mi5sing@yandex.ru',
+                to=['sergei.dd11@gmail.com', ],
+            )
+            msg.attach_alternative(html_content, "text/html")
+
+            msg.send()
+
             return redirect('home')
         else:
             error = 'Форма заполнена не верно'
